3fd9207a0c8dea278a177b4e0741ed197777c0dc
Liesbeth Baudoin <liesbaudoin@hotmail.com>
2019-03-03 18:47:32 +0100

    updated script

--- Assets/chase.cs
+++ Assets/chase.cs
@@ -11,8 +11,7 @@ public class chase : MonoBehaviour
     public float aggroRange = 10;// afstand tussen npc en fps wanneer actie moet komen
     public float patroltime = 15;// tijd in seconden tot de npc een nieuwe destination gaat zoeken
     public GameObject[] waypoints;
-
-    float speed = 0,75f;
+    private float speed = 0.75f;
 
     int index; //current waypoint index in waypoints array
     // Start is called before the first frame update
